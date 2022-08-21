e01e75369087a6a1c0ccec9aa3e54ce26662c6b6
Daltonic <darlingtongospel@gmail.com>
2022-08-21 19:51:25 +0100

    integrates the add more button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from 'react'
 import ProjectCard from "./ProjectCard"
 
 const Projects = ({projects}) => {
+  const [end, setEnd] = useState(1)
+  const [count] = useState(1)
+
+  const [collection, setCollection] = useState([])
+
+  const getCollection = () => {
+    return projects.slice(0, end)
+  }
+
+  useEffect(() => {
+    setCollection(getCollection())
+  }, [projects, end])
+
   return (
     <div className="flex flex-col">
       <div className="flex justify-center items-center flex-wrap">
-        {projects.map((project, i) => (
+        {collection.map((project, i) => (
           <ProjectCard project={project} key={i} />
         ))}
       </div>
 
-      {projects.length > 0 ? (
+      {projects.length > 0 && projects.length > collection.length ? (
         <div className="flex justify-center items-center my-5">
           <button
             type="button"
@@ -19,6 +33,7 @@ const Projects = ({projects}) => {
             leading-tight uppercase rounded-full shadow-md hover:bg-green-700 hover:shadow-lg
             focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0
             active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
+            onClick={() => setEnd(end + count)}
           >
             Load More
           </button>
